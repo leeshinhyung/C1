4066578653f987b11a148c315ba085623ba9cf6e
leeshinhyung <113335358+leeshinhyung@users.noreply.github.com>
2022-11-07 19:50:38 +0900

    Add files via upload

---
+++ hw6.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() // 세로 = 행, 가로 = 열
+{
+	int arr[5];
+	int i, j;
+	printf("Please input five integers: ");
+
+	for (i = 0; i <5; i++)
+		scanf("%d", &arr[i]);
+
+
+	printf("Odd numbers: ");
+	for (i = 0; i < 5; i++)
+	{
+		if (arr[i] % 2 != 0)
+			printf("%d ", arr[i]);	
+	}
+	printf("\n");
+
+	printf("Even numbers: ");
+	for (i = 0; i < 5; i++)
+	{
+		if (arr[i] % 2 == 0)
+			printf("%d ", arr[i]);
+	}
+	return 0;
+}
+	
+
+	
+
